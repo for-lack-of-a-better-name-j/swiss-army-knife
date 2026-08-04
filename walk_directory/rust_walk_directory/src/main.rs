0a12f69f3729efc92f2ab0e5997ba6c0eb21e636
for-lack-of-a-better-name-j <168361532+for-lack-of-a-better-name-j@users.noreply.github.com>
2026-08-04 07:07:15 -0400

    feat: Rust directory walker

--- walk_directory/rust_walk_directory/src/main.rs
+++ walk_directory/rust_walk_directory/src/main.rs
@@ -1,4 +1,25 @@
-use walkdir::WalkDir;
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    error::Error,
+    path::{Path, PathBuf},
+};
+
+use walkdir::{DirEntry, WalkDir};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Get the 1st argument (index 1), or default to "."
+    let target_dir = env::args()
+        .nth(1)
+        .map(PathBuf::from)
+        .unwrap_or_else(|| PathBuf::from("."));
+
+    for entry in WalkDir::new(target_dir) {
+        let entry = entry?;
+        println!("{}", entry.path().display());
+        // to check if it is a file: (do note that is_file() will return false
+        // if the user does not have sufficient permissions)
+        println!("{}", entry.path().is_file());
+    }
+
+    Ok(())
 }
