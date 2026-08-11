e576485b598fdfba9a6d296606b5ed4f77efa3f3
for-lack-of-a-better-name-j <168361532+for-lack-of-a-better-name-j@users.noreply.github.com>
2026-08-11 06:58:04 -0400

    feat: rust parse cli args

--- cli_args/rust_cli_args/src/main.rs
+++ cli_args/rust_cli_args/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{error::Error, path::PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(version, about = "CLI argument parser demonstration", long_about = None)]
+struct Cli {
+    /// Enable verbose output
+    #[arg(short, long)]
+    verbose: bool,
+    /// Count of tasks to execute [default = 1]
+    #[arg(short, long, default_value_t = 1)]
+    count: usize,
+    /// path to run tasks on
+    target: PathBuf,
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    println!("Target path: {}", args.target.display());
+    if args.verbose {
+        for idx in 0..args.count {
+            println!("Verbose pass {idx} on target: {}", args.target.display());
+        }
+    }
+    Ok(())
 }
