9a97f282bd36ca36567fbe490bf56b88ac4ee3d0
for-lack-of-a-better-name-j <168361532+for-lack-of-a-better-name-j@users.noreply.github.com>
2026-08-14 07:12:14 -0400

    feat: build print_usage and start main fn

--- cli_args/cpp_cli_args/main.cpp
+++ cli_args/cpp_cli_args/main.cpp
@@ -9,3 +9,27 @@ struct CliArgs {
   int count = 1;
   std::string_view target{};
 };
+
+void print_usage(std::string_view prog_name) {
+  std::cout << "Usage: " << prog_name << " [OPTIONS] <target>\n"
+            << "Options:\n"
+            << "  -v, --verbose   Enable verbose output\n"
+            << "  -c, --count <N> Execution passes (default: 1)\n"
+            << "  -h, --help      Display help\n";
+}
+
+int main(int argc, char *argv[]) {
+  std::string_view target;
+  CliArgs cli;
+  std::vector<std::string_view> args(argv + 1, argv + argc);
+  for (size_t i = 0; i < args.size(); ++i) {
+    std::string_view arg = args[i];
+
+    if (arg == "-h" || arg == "--help") {
+      print_usage(argv[0]);
+      return 0;
+    }
+    if (arg == "-v" || arg == "--verbose") {
+    }
+  }
+}
